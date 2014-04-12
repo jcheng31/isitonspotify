2130086724dec7f0347985ec5f0bc1e77c80f863
Jerome Cheng <jerome@ayulin.net>
2014-04-12 16:59:46 +0800

    Initial implementation.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,36 @@
 $(function() {
-	var spotifySearchEndpoint = "http://ws.spotify.com/search/1/track.json?q=";
-	
-	var viewModel = {
-		givenTrackName: ko.observable(""),
-		trackTitle: ko.observable(""),
-		trackArtist: ko.observable(""),
-		trackAvailability: ko.observable("")
-	};
+    var spotifySearchEndpoint = "http://ws.spotify.com/search/1/track.json?q=";
 
-	ko.applyBindings(viewModel);
+    var viewModel = {
+        givenTrackName: ko.observable(""),
+        trackTitle: ko.observable(""),
+        trackArtist: ko.observable(""),
+        trackAvailability: ko.observableArray([]),
+        getTrackAvailability: function(formElement) {
+            var searchTargetUrl = spotifySearchEndpoint + this.givenTrackName();
+            var that = this;
+            $.ajax(searchTargetUrl).done(function(data) {
+                var retrievedTracks = data.tracks.length > 0;
+                if (!retrievedTracks) {
+                    return;
+                }
+
+                var track = data.tracks[0];
+                var trackTitle = track.name;
+                var trackArtist = track.artists[0].name;
+
+                var album = track.album;
+                var rawAvailabilityString = album.availability.territories;
+                var availability = rawAvailabilityString.split(" ");
+                that.updateTrackDetails(trackTitle, trackArtist, availability);
+            });
+        },
+        updateTrackDetails: function(trackTitle, trackArtist, trackAvailability) {
+            this.trackTitle(trackTitle);
+            this.trackArtist(trackArtist);
+            this.trackAvailability(trackAvailability);
+        }
+    };
+
+    ko.applyBindings(viewModel);
 });
